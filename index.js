9265b9aaa12117e388d0e1df0a048b6e9eb04e02
Jack Lingle <captainjack0818@gmail.com>
2024-03-20 18:33:52 -0400

    implement express server

--- index.js
+++ index.js
@@ -1,85 +1,21 @@
-const { createClient } = require('@supabase/supabase-js')
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const addAllAverages = require('./addAllAverages');
 
-const supabase = createClient("https://ozkykrmuikqgrjfwemus.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96a3lrcm11aWtxZ3JqZndlbXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1NTgzOTQsImV4cCI6MjAyNDEzNDM5NH0.uw5P0WARTEleBzhNtraLsCEO2wp781rSzggxDI9dm8s");
+const app = express();
 
-const fetchTeamData = async () => {
-    return supabase.from('Scout_Data').select("Event, team, Auto_Amp_Made, Auto_Speaker_Made, Taxi, Teleop_Amp_Made, Teleop_Speaker_Made, Endgame, Trap");
-}
+app.use(cors());
 
-const average = (oldAvg, newVal, denom) => {
-    return ((oldAvg * (denom - 1) + newVal) / denom).toFixed(2);
-}
-
-const addAverages = (teamNumber, event, teamPoints) => {
-    const avg = {
-        teamNumber,
-        event,
-        matchesPlayed: 0,
-        teleAmp: 0,
-        teleSpeaker: 0,
-        autoAmp: 0,
-        autoSpeaker: 0,
-        climb: 0,
-        trapPercent: 0,
-        taxiPercent: 0,
-    };
-    for(let i = 0; i < teamPoints.length; i++) {
-        const match = teamPoints[i];
-        let climbPoints = 0;
-        if(match.Endgame === "Parked") {
-            climbPoints = 1;
-        } else if (match.Endgame === "Climbed") {
-            climbPoints = 3;
-        } else if (match.Endgame === 'Not Attempted') {
-            climbPoints = 0;
-        } else {
-            climbPoints = 5;
-        }
-        avg.matchesPlayed++;
-        avg.teleAmp = average(avg.teleAmp, match.Teleop_Amp_Made, i + 1);
-        avg.teleSpeaker = average(avg.teleSpeaker, match.Teleop_Speaker_Made, i + 1);
-        avg.autoAmp = average(avg.autoAmp, match.Auto_Amp_Made, i + 1);
-        avg.autoSpeaker = average(avg.autoSpeaker, match.Auto_Speaker_Made, i + 1);
-        avg.climb = average(avg.climb, climbPoints, i + 1);
-        avg.trapPercent = average(avg.trapPercent, match.Trap === "Successful" ? 100 : 0, i + 1);
-        avg.taxiPercent = average(avg.taxiPercent, match.Taxi ? 100 : 0, i + 1);
+app.post('/updateAverages/:eventName', async (req, res, next) => {
+    try {
+        await addAllAverages(req.params.eventName);
+        return res.status(201);
+    } catch (error) {
+        return res.status(400).send(error.message);
     }
+})
 
-    return supabase.from('Averages').insert(avg);
-
-}
-
-const addAllAverages = async (competition) => {
-    const { data, error} = await fetchTeamData();
-    if(error) return console.error(error);
-
-    const resp = await fetch("https://www.thebluealliance.com/api/v3/event/" + competition + "/teams/simple",
-        {
-            method: "GET",
-            headers: {
-                "X-TBA-Auth-Key":
-                    "3MbBFKbSOrahWa5SA7GmFv6L9ByIly1nk0vUPPSK1xQnI4ccLvsF5FRknNFz1CAm",
-            },
-        });
-    if(!resp.ok) {
-        return console.error(resp.status)
-    }
-
-    const jsonResp = await resp.json();
-    const teamsArray = jsonResp.map((d) => d.team_number.toString());
-
-    console.log(teamsArray);
-
-    for(let i = 0; i < teamsArray.length; i++) {
-        const teamData = data.filter((d) => d.team === teamsArray[i]);
-
-        const averageData = await addAverages(teamsArray[i], '2024vabla', teamData);
-        if(averageData.error) {
-            console.error('fail');
-            return;
-        }
-        console.log('success');
-    }
-}
-
-addAllAverages('2024vabla');
+app.listen(process.env.PORT, () => {
+    console.log(`Server started at ${process.env.PORT}`)
+})
